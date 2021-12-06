5a165e5b4fc21587739f4937ed214c3e0a5294a0
Osto <ostmannods@gmail.com>
2021-12-06 12:42:11 +0100

    fart

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,19 @@
-let staff = {
+let staff = [
+    {
     name: "Herko Nilsson",
     title: "Head of HR",
     info: "likes dinosaurs and pasta",
     image: "herko.jpg"
     },
     {
-        name: "Göran Persson",
-        title: "Head of Banking",
-        info: "I don't like U-countries, like Sweden for example",
-        image: "GP.jpg"
-        },
-        {
-            name: "Tord Yvel",
-            title: "Head of Poetry",
-            info: "I like stuff like farts and poop",
-            image: "Tord.jpg"
-            },
+    name: "Göran Persson",
+    title: "Head of Banking",
+    info: "I don't like U-countries, like Sweden for example",
+    image: "GP.jpg"
+    },
+    {
+    name: "Tord Yvel",
+    title: "Head of Poetry",
+    info: "I like stuff like farts and poop",
+    image: "Tord.jpg"
+    }]
