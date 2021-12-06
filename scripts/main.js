2d03274f27b31043395e297241cb757dec2f3611
Osto <ostmannods@gmail.com>
2021-12-06 12:40:48 +0100

    staffs

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+let staff = {
+    name: "Herko Nilsson",
+    title: "Head of HR",
+    info: "likes dinosaurs and pasta",
+    image: "herko.jpg"
+    },
+    {
+        name: "Göran Persson",
+        title: "Head of Banking",
+        info: "I don't like U-countries, like Sweden for example",
+        image: "GP.jpg"
+        },
+        {
+            name: "Tord Yvel",
+            title: "Head of Poetry",
+            info: "I like stuff like farts and poop",
+            image: "Tord.jpg"
+            },
